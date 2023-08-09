b4e9336474b352a8c863c9a5adf410f4597175e4
Belle Xu <bellex22@protonmail.com>
2023-08-08 20:59:21 -0400

    finish step1

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,24 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
+
+    const chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+      if (!(this.words[i] in chains)) {
+        chains[this.words[i]] = [this.words[i + 1] || null] ;
+      }
+      else {
+        chains[this.words[i]].push(this.words[i + 1] || null);
+      }
+    }
+
+    return chains;
+
+
   }
 
 
@@ -40,5 +53,25 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    let text = this.words[0];
+    let subString = this.words[0];
+    while (subString !== null) {
+      let key = subString;
+      // console.log("the key is", key);
+      let selectedIdx = Math.floor(Math.random() * this.chains[key].length);
+      subString = this.chains[key][selectedIdx];
+      if (subString !== null) text += (" " + subString);
+    }
+
+    return text;
+
+
   }
 }
+
+
+module.exports = {
+  MarkovMachine,
+};
+
